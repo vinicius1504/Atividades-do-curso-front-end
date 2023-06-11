7445e6ac924b5f6dfa017ff24a000fc01652bde2
꧁ঔৣ☬vini☬ঔৣ꧂ <vgbl1504@gmail.com>
2023-06-11 18:33:44 -0400

    .

--- pet-store/login.js
+++ pet-store/login.js
@@ -1,9 +1,5 @@
 var login = document.getElementById("login")
 var btn = document.getElementById("cad")
 
-function cad(){
-    while (login.removeChild("login"));
-    console.log("ola");
-}
+login.removeChild(btn.firstChild);
 
-btn.onclick = cad
