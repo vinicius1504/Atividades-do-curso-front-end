81f634df6d4c7a72bc877a364892387a92fb1441
꧁ঔৣ☬vini☬ঔৣ꧂ <vgbl1504@gmail.com>
2023-06-26 14:22:02 -0400

    .

--- pet-store/pag_principal/pag_principal.js
+++ pet-store/pag_principal/pag_principal.js
@@ -78,35 +78,54 @@ const data_end =
       {
          "id": 5,
          "nome": "Ração Pedigree para Cães Adultos",
-         "imgpd": "/pet-store/produtos/biscrok.png",
+         "imgpd": "/pet-store/produtos/large_f2612a6c-21ca-4256-a0e4-0f78a9b0c652_1k.png",
+         "valor": "R$ 25,00"
+      },
+      {
+         "id": 5,
+         "nome": "Ração Whiskas Sabor Frango 1,5Kg",
+         "imgpd": "/pet-store/produtos/imagem_2023-06-26_135706773-PhotoRoom.png-PhotoRoom.png",
+         "valor": "R$ 15,00"
+      },
+      {
+         "id": 6 ,
+         "nome": "Shampoo para Cães e Gatos 500ml",
+         "imgpd": "/pet-store/produtos/imagem_2023-06-26_092301914-PhotoRoom.png-PhotoRoom.png",
+         "valor": "R$ 12,00"
+      },
+      {
+         "id": 7,
+         "nome": " Brinquedo Interativo para Cães",
+         "imgpd": "/pet-store/produtos/brinquedo_interativo.png",
+         "valor": "R$ 8,00"
+      },
+      {
+         "id": 8,
+         "nome": "Areia Sanitária para Gatos 5Kg",
+         "imgpd": "/pet-store/produtos/areia.png",
          "valor": "R$ 10,00"
       },
       {
          "id": 5,
-         "nome": "Ração Pedigree para Cães Adultos",
-         "imgpd": "/pet-store/produtos/biscrok.png",
-         "valor": "R$ 10,00"
+         "nome": "Cama para gatos Cat Bed",
+         "imgpd": "/pet-store/produtos/imagem_2023-06-26_134857326-PhotoRoom.png-PhotoRoom.png",
+         "valor": "R$ 100,00"
       },
       {
          "id": 5,
-         "nome": "Ração Pedigree para Cães Adultos",
-         "imgpd": "/pet-store/produtos/biscrok.png",
-         "valor": "R$ 10,00"
+         "nome": "Casa para Gato Arranhador com Rede Bege Lilies Móveis",
+         "imgpd": "/pet-store/produtos/imagem_2023-06-26_135223444-PhotoRoom.png-PhotoRoom.png",
+         "valor": "R$ 155,00"
       },
       {
          "id": 5,
-         "nome": "Ração Pedigree para Cães Adultos",
-         "imgpd": "/pet-store/produtos/biscrok.png",
-         "valor": "R$ 10,00"
+         "nome": "Osso de Boi",
+         "imgpd": "/pet-store/produtos/brinquedo_interativo.png",
+         "valor": "R$ 8,00"
       },
-      {
-         "id": 5,
-         "nome": "Ração Pedigree para Cães Adultos",
-         "imgpd": "/pet-store/produtos/biscrok.png",
-         "valor": "R$ 10,00"
-      }
 
       
       
    ]
 }
+
